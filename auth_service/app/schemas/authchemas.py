4c54eaa0db81f9921a7121182f46a6da9e62edc0
zen-patilrohit <patil.rohit14@gmail.com>
2025-09-12 13:36:35 +0530

    auth reponse changed

--- auth_service/app/schemas/authchemas.py
+++ auth_service/app/schemas/authchemas.py
@@ -1,48 +1,34 @@
 from pydantic import BaseModel, EmailStr
 from typing import Literal, Optional, Union
 
-AllowedRole = Literal["manager", "admin", "superadmin", "user"]
+AllowedRole = Literal["manager", "admin", "superadmin", "user", "default"]
 
 # -------- Google --------
 class GoogleAuthRequest(BaseModel):
     token: str
 
-class RegisterRequest(BaseModel):
-    email: EmailStr
-    role: AllowedRole
-
-class Token(BaseModel):
-    access_token: str
-    token_type: str = "bearer"
-    redirect_url: Optional[str] = None
-
-class NotRegisteredResponse(BaseModel):
-    needs_registration: bool = True
-    email: EmailStr
-    allowed_roles: list[AllowedRole]
-
-class LoginSuccessResponse(BaseModel):
-    needs_registration: bool = False
-    token: Token
-
-GoogleAuthResponse = Union[NotRegisteredResponse, LoginSuccessResponse]
 
 # -------- Mobile --------
-class VerifyResponse(BaseModel):
-    needs_registration: bool
-    access_token: Optional[str] = None
-    token_type: Optional[str] = "bearer"
-    redirect_url: Optional[str] = None
     
-    class Config:
-        from_attributes = True
-    
-
-
 class MobileRequest(BaseModel):
     mobile: str
 
 class OTPVerify(BaseModel):
     mobile: str
     otp: str
-    role: Optional[AllowedRole] = None
+
+
+# -------Common----------
+class NotRegisteredResponse(BaseModel):
+    needs_registration: bool = True
+    email: Optional[EmailStr] = None
+    mobile: Optional[str] = None
+    allowed_roles: list[AllowedRole]
+
+class LoginSuccessResponse(BaseModel):
+    needs_registration: bool = False
+    access_token: str
+    token_type: str = "bearer"
+    redirect_url: Optional[str] = None
+
+AuthResponse = Union[NotRegisteredResponse, LoginSuccessResponse]

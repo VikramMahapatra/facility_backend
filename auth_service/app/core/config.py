52c6bdb2560ef87f522e88b245e159d6de53ce33
zen-patilrohit <patil.rohit14@gmail.com>
2025-09-13 22:55:31 +0530

    Refactor-2: use env vars for Twilio credentials

---
+++ auth_service/app/core/config.py
@@ -0,0 +1,32 @@
+import os
+from pydantic_settings import BaseSettings
+from dotenv import load_dotenv
+import os
+
+# Always load .env from root
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+load_dotenv(os.path.join(BASE_DIR, ".env"))
+
+DB_USER = os.getenv("DB_USER")
+DB_PASS = os.getenv("DB_PASS")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+
+SQLALCHEMY_DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+
+class Settings(BaseSettings):
+    GOOGLE_CLIENT_ID: str = os.getenv("GOOGLE_CLIENT_ID")
+
+    JWT_SECRET: str = os.getenv("JWT_SECRET")
+    JWT_ALGORITHM: str = "HS256"
+    JWT_EXPIRE_MINUTES: int = 1440  # 24 hours default
+
+    # Twilio
+    TWILIO_ACCOUNT_SID: str | None = os.getenv("TWILIO_ACCOUNT_SID")
+    TWILIO_AUTH_TOKEN: str | None = os.getenv("TWILIO_AUTH_TOKEN")
+    TWILIO_VERIFY_SID: str | None = os.getenv("TWILIO_VERIFY_SID")
+    UPLOAD_DIR : str | None = os.getenv("PROFILE_PIC_PATH")
+
+settings = Settings()

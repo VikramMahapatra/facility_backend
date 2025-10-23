6a87b95ba2055e1d32cf7f0ae02bc2b3e1538c82
zen-patilrohit <patil.rohit14@gmail.com>
2025-10-24 00:06:41 +0530

    export functionality

---
+++ facility_service/app/router/common/export_router.py
@@ -0,0 +1,37 @@
+from datetime import datetime
+from typing import Any, List, Optional
+from fastapi import APIRouter, Depends, Query, Request, Response
+from fastapi.responses import StreamingResponse
+from sqlalchemy.orm import Session
+from shared.database import get_facility_db as get_db
+from shared.auth import validate_current_token
+from shared.schemas import ExportRequestParams, ExportResponse, Lookup, UserToken
+from ...crud.common import export_crud as crud
+
+
+router = APIRouter(
+    prefix="/api/export",
+    tags=["Export"],
+    dependencies=[Depends(validate_current_token)]
+)
+
+
+# @router.get(
+#     "/",
+#     response_class=StreamingResponse,
+#     responses={
+#         200: {
+#             "content": {
+#                 "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet": {}
+#             },
+#             "description": "Excel file download",
+#         }
+#     },
+# )
+@router.get("/", response_model=ExportResponse)
+def get_export_data(
+        type: str,
+        params: ExportRequestParams = Depends(),
+        db: Session = Depends(get_db),
+        current_user: UserToken = Depends(validate_current_token)):
+    return crud.get_export_data(db, current_user.org_id, type, params)

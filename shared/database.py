acc1f02545923d60d73b7e2a7e08f67ba077b537
zen-patilrohit <patil.rohit14@gmail.com>
2025-10-24 16:16:09 +0530

    database connection issue

--- shared/database.py
+++ shared/database.py
@@ -7,27 +7,43 @@ AuthBase = declarative_base()
 Base = declarative_base()
 
 # Auth DB
-auth_engine = create_engine(AUTH_DATABASE_URL,pool_pre_ping=True,pool_recycle=300)
-AuthSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=auth_engine)
+auth_engine = create_engine(
+    AUTH_DATABASE_URL,
+    pool_pre_ping=True,
+    pool_recycle=300,
+    pool_size=5,          # max idle connections
+    max_overflow=10,      # max temporary extra connections
+    pool_timeout=30       # wait time before failing
+)
+AuthSessionLocal = sessionmaker(
+    autocommit=False, autoflush=False, bind=auth_engine)
 
 # Facility DB
-facility_engine = create_engine(FACILITY_DATABASE_URL,pool_pre_ping=True,pool_recycle=300)
-FacilitySessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=facility_engine)
+facility_engine = create_engine(
+    FACILITY_DATABASE_URL,
+    pool_pre_ping=True,
+    pool_recycle=300,
+    pool_size=5,
+    max_overflow=10,
+    pool_timeout=30
+)
+FacilitySessionLocal = sessionmaker(
+    autocommit=False, autoflush=False, bind=facility_engine)
 
 # Dependency
+
+
 def get_auth_db():
     db = AuthSessionLocal()
     try:
         yield db
     finally:
         db.close()
-    
+
+
 def get_facility_db():
     db = FacilitySessionLocal()
     try:
         yield db
     finally:
         db.close()
-        
-        
-

5e5c81f1985db2e44619a100e46a336d97bfb8ec
prachibangre <prachibangre100@gmail.com>
2025-11-10 12:30:37 +0530

    adding new router for get ticket details by id

--- facility_service/app/router/service_ticket/tickets_router.py
+++ facility_service/app/router/service_ticket/tickets_router.py
@@ -5,7 +5,7 @@ from typing import List
 from facility_service.app import crud
 from shared.core.schemas import UserToken
 from ...crud.service_ticket import tickets_crud as crud
-from ...schemas.service_ticket.tickets_schemas import TicketCreate, TicketFilterRequest, TicketOut
+from ...schemas.service_ticket.tickets_schemas import TicketCreate, TicketDetailsResponse, TicketFilterRequest, TicketOut
 from shared.core.database import get_auth_db, get_facility_db as get_db
 from shared.core.auth import validate_current_token
 from shared.helpers.json_response_helper import success_response
@@ -36,3 +36,15 @@ def get_tickets(
     current_user: UserToken = Depends(validate_current_token)
 ):
     return crud.get_tickets(db, params, current_user)
+
+@router.get("/tickets/{ticket_id}", response_model=TicketDetailsResponse)
+def get_ticket_details_route(
+    ticket_id: str,
+    db: Session = Depends(get_db),
+    auth_db: Session = Depends(get_auth_db),
+    current_user: UserToken = Depends(validate_current_token)
+):
+    """
+    Get complete ticket details for a given ticket_id
+    """
+    return crud.get_ticket_details(db, auth_db, ticket_id)

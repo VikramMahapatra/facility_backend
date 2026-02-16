32ac00f60dc7efb174c0a66590cc453c5d3148a3
zen-patilrohit <patil.rohit14@gmail.com>
2026-02-16 19:36:38 +0530

    parking slot changes

--- facility_service/app/models/parking_access/parking_slots.py
+++ facility_service/app/models/parking_access/parking_slots.py
@@ -19,7 +19,7 @@ class ParkingSlot(Base):
     slot_no = Column(String(20), nullable=False)  # P12, B2-45 etc
     slot_type = Column(String(20))
     # covered | open | visitor | handicapped | ev
-    is_deleted = Column(Boolean, default=True)
+    is_deleted = Column(Boolean, default=False, nullable=False)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
     updated_at = Column(DateTime(timezone=True),
                         server_default=func.now(), onupdate=func.now())
